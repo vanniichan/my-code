faf258f2c9553c27ea81bc385e20dc61a3319828
Minh++ <92814976+BuiDucMinh23@users.noreply.github.com>
2022-03-25 17:56:43 +0700

    c++

--- test2-3.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-int n;cin>>n;
-while(n--){
-int64_t a,b,c=0;cin>>a>>b;  int e=b%a;int64_t f=b/a;
-c=(f+1)*(f+1)*e+(a-e)*f*f;cout<<c<<endl;  
-}}
