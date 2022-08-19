9287b64819959bf3c3a37194fb84cba7a85b1c1d
Minh++ <92814976+BuiDucMinh23@users.noreply.github.com>
2022-08-19 09:54:13 +0700

    Update bai2.cpp

--- công nam/bai2.cpp
+++ công nam/bai2.cpp
@@ -1,48 +1,85 @@
-#include <stdio.h>
-#include <string.h>
-int string_length(char s[]) {
-   int i=0;
-   while(s[i]!='\0')
-      i++;
-   return i;    
+#include<stdio.h>
+#include<stdlib.h>
+void nhapmang(int &n,int a[]){
+	int i;scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
 }
-void string_reverse(char str[]) {
-   int i, j, len;
-   char ch;
-   j = len = string_length(str) - 1;
-   i = 0;
-   while(i < j) {
-      ch = str[j];
-      str[j] = str[i];
-      str[i] = ch;
-      i++;
-      j--;
-   }
+int timkiem(int n,int a[]){
+	int max=-1e9,i;
+	for(i=0;i<n;i++){
+		if(a[i]%2==0&&a[i]>max&&a[i]<0) max=a[i];
+	}
+	return max;
 }
-int main () {
-   char s1[1000];gets(s1);
-   char reverse[100] = "";
-   char temp[50];
-   int i, j, n;
- 
- 
-   n = string_length(s1);
- 
-   for(i = n-1; i >= 0; --i) {
- 
-      for(j = 0; i >= 0 && s1[i] != ' '; --i,++j)
-         temp[j] = s1[i];
- 
-      temp[j] = '\0';
- 
-      string_reverse(temp);
-       
-      strcat(reverse, temp);
-      strcat(reverse, " ");
+int tinhtongodd(int n,int a[]){
+	int i,sum=0;
+	for(i=0;i<n;i++){
+		if(a[i]%2!=0) sum+=a[i];
+	}
+	return sum;
+}
+int gcd(int a,int b){
+      int res = 0;
+      while (b > 0){
+         int temp = b;
+         b = a % b;
+         a = temp;
+         res = a;
+      }
+      return res;
    }
- 
-   
-   printf("%s",reverse);
- 
-   return 0;
+int tinhgcd(int n,int a[]){
+	int i;int gd=a[0];
+	for(i=0;i<n;i++){
+		gd=gcd(gd,a[i]);
+	}
+	return gd;
+}
+void sortandout(int n,int a[]){
+	int i,j,swap;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+            if(a[i]<a[j]){
+				swap=a[i];
+				a[i]=a[j];
+				a[j]=swap;
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
+int main(){
+int n,a[1000];
+
+int m;
+scanf("%d",&m);
+switch (m)
+{
+case 1:
+	nhapmang(n,a);
+	break;
+case 2:
+nhapmang(n,a);
+    printf("%d",timkiem(n,a));
+	break;
+case 3:
+nhapmang(n,a);
+    printf("%d",tinhtongodd(n,a));
+	break;
+case 4:
+nhapmang(n,a);
+    printf("%d",tinhgcd(n,a));
+	break;
+case 5:
+nhapmang(n,a);
+sortandout(n,a);
+break;
+default:
+system("pause");
+	break;
+}
 }
